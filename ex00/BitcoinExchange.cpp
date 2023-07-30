aa21deab70f4bc3265548fcb92940692858563de
Carlos Orellana <corellana1988@gmail.com>
2023-07-30 19:28:33 +0300

    more progress

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: corellan <corellan@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/29 14:41:17 by corellan          #+#    #+#             */
-/*   Updated: 2023/07/29 15:32:31 by corellan         ###   ########.fr       */
+/*   Updated: 2023/07/30 19:28:11 by corellan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,18 @@
 
 BitcoinExchange::BitcoinExchange(std::string &input_file)
 {
+	this->_parsed_database = NULL;
+	this->_parsed_file = NULL;
 	this->_database.open("data.csv", std::ifstream::in);
 	if (this->_database.fail() == true)
 		throw (ErrorOpeningDatabase());
 	this->_file.open(input_file, std::ifstream::in);
 	if (this->_file.fail() == true)
 		throw (ErrorOpeningFile());
+	if (this->_checkFormatDatabase() == -1)
+		throw (WrongFormatDatabase());
+	if (this->_checkFormatFile() == -1)
+		throw (WrongFormatFile());
 	return ;
 }
 
@@ -27,7 +33,75 @@ BitcoinExchange::~BitcoinExchange(void)
 {
 	this->_database.close();
 	this->_file.close();
+	if (this->_parsed_database)
+		delete [] this->_parsed_database;
+	if (this->_parsed_file)
+		delete [] this->_parsed_file;
 	return ;
+}
+
+int	BitcoinExchange::_checkFormatFile(void)
+{
+	if (this->_storeDoc(this->_file, this->_file_doc, this->_lines_file) == -1)
+		return (-1);
+	
+}
+
+int	BitcoinExchange::_storeDoc(std::ifstream &file, std::string &doc, int &lines)
+{
+	std::string	temp;
+	int			flag;
+
+	flag = 0;
+	lines = 0;
+	while(1)
+	{
+		std::getline(file, temp);
+		if (file.eof() == true)
+			break ;
+		if (file.fail() == true)
+		{
+			flag = 2;
+			break ;
+		}
+		if (flag == 0)
+		{
+			doc = temp;
+			flag = 1;
+		}
+		else
+			doc += temp;
+		lines++;
+		doc += '\n';
+		file.sync();
+	}
+	file.sync();
+	if (flag == 2)
+		return (-1);
+	return (0);
+}
+
+int	BitcoinExchange::_parseLines(std::string *ptr, std::string &doc, int &lines)
+{
+	int	i;
+	int	pos;
+
+	i = 0;
+	pos = 0;
+	try
+	{
+		ptr = new std::string[lines];
+	}
+	catch(const std::exception& e)
+	{
+		return (-1);
+	}
+	while (doc.find('\n') != std::string::npos)
+	{
+		pos = doc.find('\n');
+		ptr[i] = doc.substr(0, pos);
+		i++;
+	}
 }
 
 const char	*BitcoinExchange::ErrorOpeningDatabase::what(void) const throw()
@@ -38,4 +112,14 @@ const char	*BitcoinExchange::ErrorOpeningDatabase::what(void) const throw()
 const char	*BitcoinExchange::ErrorOpeningFile::what(void) const throw()
 {
 	return ("Error: there was a problem trying to open the file provided as argument.");
+}
+
+const char	*BitcoinExchange::WrongFormatDatabase::what(void) const throw()
+{
+	return ("Error: file data.csv does not have the proper format.");
+}
+
+const char	*BitcoinExchange::WrongFormatFile::what(void) const throw()
+{
+	return ("Error: file passed as argument does not have the proper format.");
 }
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: corellan <corellan@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/29 14:50:32 by corellan          #+#    #+#             */
-/*   Updated: 2023/07/29 15:17:34 by corellan         ###   ########.fr       */
+/*   Updated: 2023/07/30 16:28:41 by corellan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define BITCOINEXCHANGE_HPP
 
 #include <map>
+#include <cstdlib>
 #include <fstream>
 #include <stdexcept>
 
@@ -33,7 +34,18 @@ public:
 
 		virtual const char	*what(void) const throw();
 	};
+	class	WrongFormatDatabase : public std::exception
+	{
+	public:
 
+		virtual const char	*what(void) const throw();
+	};
+	class	WrongFormatFile : public std::exception
+	{
+	public:
+
+		virtual const char	*what(void) const throw();
+	};
 	BitcoinExchange(std::string &input_file);
 	~BitcoinExchange(void);
 
@@ -42,11 +54,22 @@ private:
 	std::map<std::string, float>	_map_database;
 	std::ifstream					_database;
 	std::ifstream					_file;
+	int								_lines_database;
+	int								_lines_file;
+	std::string						_database_doc;
+	std::string						_file_doc;
+	std::string						*_parsed_database;
+	std::string						*_parsed_file;
 
 	BitcoinExchange(void);
 	BitcoinExchange(BitcoinExchange const &rhs);
 
 	BitcoinExchange	operator=(BitcoinExchange const &rhs);
+
+	int		_checkFormatDatabase(void);
+	int		_checkFormatFile(void);
+	int		_storeDoc(std::ifstream &file, std::string &doc, int &lines);
+	int		_parseLines(std::string *ptr, std::string &doc, int &lines);
 };
 
 #endif
